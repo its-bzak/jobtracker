0100fffebe89a05de7b302999a807a57b4075634
Ryan B <barczakryan@outlook.com>
2025-12-14 23:25:56 -0500

    Added to Viewsets so only applications visible belong to the applicant who created them. Similarly, interviews only visible to the applicant that the application belongs to.

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
-from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from .views import JobPostingViewSet, ApplicationViewSet, InterviewViewSet
+
+router = DefaultRouter()
+router.register(r'job-postings', JobPostingViewSet, basename='job-postings')
+router.register(r'applications', ApplicationViewSet, basename='applications')
+router.register(r'interviews', InterviewViewSet, basename='interviews')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,2 +1,53 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from django.db.models import Q
+from .models import JobPosting, Application, Interview
+from .serializers import (JobPostingSerializer, ApplicationSerializer, InterviewSerializer)
+
 # Create your views here.
+
+class JobPostingViewSet(viewsets.ModelViewSet):
+    queryset = JobPosting.objects.all()
+    serializer_class = JobPostingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(company=self.request.user)
+
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    serializer_class = ApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Application.objects.filter(
+            Q(applicant=user)
+        ).distinct()
+
+    def perform_create(self, serializer):
+        serializer.save(applicant=self.request.user)
+
+    def perform_update(self, serializer):
+        application = self.get_object()
+        if application.applicant != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this application.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.applicant != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this application.")
+        instance.delete()
+
+
+
+class InterviewViewSet(viewsets.ModelViewSet):
+    serializer_class = InterviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Interview.objects.filter(
+            Q(application__applicant=user)
+        ).distinct()
