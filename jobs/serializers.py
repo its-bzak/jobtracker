a07d654a165712379b3f6639489844824db78eb7
Ryan B <barczakryan@outlook.com>
2025-12-14 20:47:45 -0500

    Created Interview serializer

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
-from .models import JobPosting, Application
+from .models import Interview, JobPosting, Application
 
 class JobPostingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JobPosting
-        fields = ['id', 'title', 'company_name', 'location', 'salary_range', 'description', 'posted_date', 'employment_type']
+        fields = ['id', 'title', 'company_name', 'location', 'employment_means', 'salary_range', 'description', 'posted_date', 'employment_type']
 
 class ApplicationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Application
         fields = ['id', 'applicant', 'job', 'application_date', 'status']
+
+    def validate(self, data):
+        
+        # Check that the applicant has not already applied for the same job.
+        
+        applicant = self.context['request'].user
+        job = data.get('job')
+        if Application.objects.filter(applicant=applicant, job=job).exists():
+            raise serializers.ValidationError("You have already applied for this job.")
+        return data
+    
+class InterviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Interview
+        fields = ['id', 'application', 'interview_date', 'interviewer_name', 'notes', 'means_of_interview']
